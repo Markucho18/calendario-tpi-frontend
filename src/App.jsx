69aa07230b94276b63b021ce72b498d174567fc0
Markucho18 <marcossosa21415@gmail.com>
2024-12-14 09:01:06 -0300

    Proyecto inicializado y dependencias

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles/App.module.css"
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+        <p>Hola mundo</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa autem distinctio rerum iste amet nobis optio maiores quas doloremque facere.</p>
+    </div>
+  )
+}
+
+export default App

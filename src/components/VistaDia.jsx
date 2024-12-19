d62d415c20bd23db17766b67414b23c6eed156db
Markucho18 <marcossosa21415@gmail.com>
2024-12-19 13:34:33 -0300

    Header sticky en tablero

--- src/components/VistaDia.jsx
+++ src/components/VistaDia.jsx
@@ -2,16 +2,21 @@ import styles from "../styles/VistaDia.module.css"
 
 const VistaDia = () => {
   return (
-    <main className={styles.contenedorTablero}> 
-      <aside>
-        {Array.from({length: 12}).map((_,i) => (
-          <p>{`${i + 1}AM`}</p>
-        ))}
-        {Array.from({length: 12}).map((_,i) => (
-          <p>{`${i + 1}PM`}</p>
-        ))}
-      </aside>
-      <div className={styles.tableroDia}></div>
+    <main className={styles.contenedorVista}> 
+      <header>
+        Holanda
+      </header>
+      <div className={styles.contenedorTablero}>
+        <aside>
+          {Array.from({length: 12}).map((_,i) => (
+            <p key={i}>{i == 0 ? "" : `${i} AM`}</p>
+          ))}
+          {Array.from({length: 15}).map((_,i) => (
+            <p key={i}>{i === 14 ? "" : `${i === 0 ? 12 : i } PM`}</p>
+          ))}
+        </aside>
+        <div className={styles.tableroDia}></div>
+      </div>
     </main>
   )
 }
